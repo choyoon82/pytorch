2c992d0e22a10820cfbc7d75cb53294d62403510
sixtick_home <choyoon8282@gmail.com>
2024-10-08 23:18:12 +0900

    .

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import streamlit as st # pip install streamlit
 import numpy as np
 
-
-def show_numpy():
-    pass
-        
-    code = st.code('''
-        nparr = np.random.randint(2,10,5)
-        print(nparr)
-        ''')
-    if st.button('실행'):
-        pass
-        st.text_area(eval(code))
-
 with st.sidebar.expander('PYTHON'):
     page = st.radio(
         "",
         ["numpy"],
         index=None,
     )
-    if page == 'numpy':
-        show_numpy()
 
+if page == 'numpy':
+    code = st.code('''nparr = np.random.randint(2,10,5)
+        print(nparr)
+        ''')
+    if st.button('실행'):
+        st.text_area(exec(code))
 
 
 st.text('hello world')
