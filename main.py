78aeec3a2632eaf7429de8d4905f9bb842dd2038
Young Hoon Cho <hungh4@naver.com>
2024-10-08 22:05:26 +0900

    test numpy

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import streamlit as st
+import streamlit as st # pip install streamlit
+import numpy as np
+
+with st.sidebar.expander('PYTHON'):
+    page = st.radio(
+        "",
+        ["numpy"],
+        index=None,
+    )
+    if page == 'numpy':
+        pass
+        
+        code = st.code('''
+            nparr = np.random.randint(2,10,5)
+            print(nparr)
+            ''')
+        if st.button('실행'):
+            pass
+            st.text_area(eval(code))
+
+
 
 st.text('hello world')
